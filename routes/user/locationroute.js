5f2b8c4e8b3e9eba6d21c5a525ba539bc4ecfabf
tarunjolly <tarunjolly8@gmail.com>
2020-08-05 01:04:36 +0530

    shhh
    Signed-off-by: tarunjolly <tarunjolly8@gmail.com>

--- routes/user/locationroute.js
+++ routes/user/locationroute.js
@@ -8,9 +8,8 @@ const sequelize = require('sequelize');
 
 
 
-route.post('/getvendors',(req,res)=>{
+route.get('/getvendors',(req,res)=>{
   
-
     db.query("SELECT markers.vendorVendorId,vendors.name,vendors.localityOfStall,vendors.status,vendors.image, ( 6371 * acos( cos( radians(37) ) * cos( radians( :lat ) ) * cos( radians( :lng ) - radians(-122) ) + sin( radians(37) ) * sin( radians( :lat ) ) ) ) AS distance FROM markers INNER JOIN  vendors ON markers.vendorVendorId=vendors.vendorId HAVING distance < 2 ORDER BY distance LIMIT 0 , 20;",{
         replacements: {lat: req.body.lat,lng:req.body.lng},
         type:sequelize.QueryTypes.SELECT
